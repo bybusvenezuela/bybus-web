dc4ec646ffb760e08058fec2bdfab498dff9fe50
chrisesbueno <alvarezchristopherve@gmail.com>
2023-11-02 16:16:10 -0400

    Cambios

---
+++ src/pages/home/support/index.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+import React from "react";
+import styles from "@/styles/Support.module.css";
+
+const Support = () => {
+  return (
+    <div className={styles.content}>
+      <Menu />
+      <div className="container section">Devoluciones</div>
+    </div>
+  );
+};
+
+export default Support;
