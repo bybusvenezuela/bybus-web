d59f102b1b5e9e450c461f46ad90a6db72dde57a
Juvelineorb4 <73950715+Juvelineorb4@users.noreply.github.com>
2024-01-03 15:42:22 -0400

    cambios

--- src/ui-components/OrderDetailUpdateForm.jsx
+++ src/ui-components/OrderDetailUpdateForm.jsx
@@ -422,9 +422,9 @@ export default function OrderDetailUpdateForm(props) {
         step="any"
         value={total}
         onChange={(e) => {
-          let value = isNaN(parseInt(e.target.value))
+          let value = isNaN(parseFloat(e.target.value))
             ? e.target.value
-            : parseInt(e.target.value);
+            : parseFloat(e.target.value);
           if (onChange) {
             const modelFields = {
               amount,
