fcd1bd9b6992f449c4ede87d15fff7f5382f2a85
chrisesbueno <alvarezchristopherve@gmail.com>
2024-01-22 11:55:35 -0400

    Cambios finales

--- src/pages/home/support/index.jsx
+++ src/pages/home/support/index.jsx
@@ -89,7 +89,7 @@ const Support = () => {
   console.log(orderTravel);
   return (
     <div className={styles.content}>
-      <Menu ancho={250} />
+      <Menu ancho={249} />
       <div className="container section">
         <div className={styles.up}>
           <h1>Devoluciones</h1>
